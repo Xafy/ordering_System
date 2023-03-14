f4095e74393ca47e78499c857ce3674b9824067a
Xafy <57871519+Xafy@users.noreply.github.com>
2023-03-14 16:46:15 +0200

    .

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum', 'is_customer'])->group(function(){
     Route::patch('update/{user}', [AuthController::class, 'updateUser']);
     Route::get('orders', [OrderController::class, 'getOrders']);
-    Route::post('order', [OrderController::class, 'createOrder']);
-    Route::post('order/{order}', [OrderController::class, 'cancelOrder']);
+    Route::post('orders', [OrderController::class, 'createOrder']);
+    Route::post('orders/{order}', [OrderController::class, 'cancelOrder']);
 });
 
 Route::post('login', [AuthController::class, 'login']);
